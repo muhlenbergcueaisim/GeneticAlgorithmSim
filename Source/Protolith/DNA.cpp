0482811da4416bf57bbfef78adbb1b0d93771c47
Ricky Morash <richardnmorash@gmail.com>
2019-02-20 18:53:34 -0500

    Fix Sintax Error in DNA
    accidentilly used () in place of [] so just fixing that

--- Source/Protolith/DNA.cpp
+++ Source/Protolith/DNA.cpp
@@ -53,10 +53,10 @@ UDNA * UDNA::Mutate(UNDA * DNA, int chance)
 	for (int i = 0; i < DNA->NumberOfGenes; i++) {
 		if (rand() % 100 < chance) {
 			if (rand() % 2 == 0) {
-				DNA->Genes(i)++;
+				DNA->Genes[i]++;
 			}
 			else {
-				DNA->Genes(i)--;
+				DNA->Genes[i]--;
 			}
 		}
 	}
@@ -100,27 +100,27 @@ TArray<UDNA*> UDNA::Cross(UDNA* parent0, UDNA* parent1)
 			}
 		}
 		mask0 = mask0 << randomNum;
-		children(0)->Genes.emplace((parent0->Genes(i) & mask0) | (parent1->Genes(i) & mask1));
-		children(1)->Genes.emplace((parent0->Genes(i) & mask1) | (parent1->Genes(i) & mask0));
+		children[0]->Genes.emplace((parent0->Genes[i] & mask0) | (parent1->Genes[i] & mask1));
+		children[1]->Genes.emplace((parent0->Genes[i] & mask1) | (parent1->Genes[i] & mask0));
 	}
 	for (int i = minLen; i < maxLen; i++) {
 		randomNum = rand() % 3;
 		if (randomNum == 0) {
-			children(0)->Genes.emplace(longPraent->Genes(i));
+			children[0]->Genes.emplace(longPraent->Genes[i]);
 			child0GeneCount++;
 		}
 		else if (randomNum == 1) {
-			children(1)->Genes.emplace(longPraent->Genes(i));
+			children[1]->Genes.emplace(longPraent->Genes[i]);
 			child1GeneCount++;
 		}
 		else {
-			children(0)->Genes.emplace(longPraent->Genes(i));
-			children(1)->Genes.emplace(longPraent->Genes(i));
+			children[0]->Genes.emplace(longPraent->Genes[i]);
+			children[1]->Genes.emplace(longPraent->Genes[i]);
 			child0GeneCount++;
 			child1GeneCount++;
 		}
 	}
-	children(0)->NumberOfGenes = child0GeneCount;
-	children(1)->NumberOfGenes = child1GeneCount;
+	children[0]->NumberOfGenes = child0GeneCount;
+	children[1]->NumberOfGenes = child1GeneCount;
 	return children;
 }
